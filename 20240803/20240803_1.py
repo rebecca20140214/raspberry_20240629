4c7792ec04d473adf5ab16ebde653a21b88d844f
rebecca20140214 <rebecca20140214@gmail.com>
2024-08-03 10:27:43 +0800

    .

--- 20240803/20240803_1.py
+++ 20240803/20240803_1.py
@@ -1,25 +1,19 @@
+
 import os.path
 import os
 from datetime import datetime
 import random
 
-
-def create_directory():
+def created_log_file():
     current_path = os.path.abspath(__name__) #取得目前檔案路徑
     directory_name = os.path.dirname(current_path) #取得目前資料夾路行
     data_path = os.path.join(directory_name,'data') #目前資料夾路徑加上data目錄
-    #print(data_path)
     if not os.path.isdir(data_path):
         print("沒有data的目錄,手動建立目錄")
         os.mkdir(data_path)
     else:
         print("目錄已經建立")
     
-    return data_path
-
-def main():
-    data_path=create_directory()
-
     log_path = os.path.join(data_path,'iot.log')
     if not os.path.isfile(log_path):
         print("沒有iot.log檔,建立新檔")
@@ -28,7 +22,11 @@ def main():
         
     else:
         print("已經有log檔")
+    
+    return log_path
+    
 
+def record_info(log_path): 
     now = datetime.now()
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
     humidity = str(random.randint(330,820) / 10)
@@ -36,5 +34,13 @@ def main():
     with open(log_path,mode='a',encoding='utf-8',newline='') as file:
         file.write(now_str + ',' + humidity + ',' + celsius + "\n")
 
-if __name__=='__main__':
+
+def main():
+    log_path = created_log_file()
+    record_info(log_path)
+    
+    
+
+
+if __name__ == '__main__':
     main()
