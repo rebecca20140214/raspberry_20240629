2bb12edfa7a7a18b236bc1560b87090c3bdbbccf
rebecca20140214 <rebecca20140214@gmail.com>
2024-08-17 15:18:37 +0800

    .

--- 20240817/receive.py
+++ 20240817/receive.py
@@ -1,9 +1,12 @@
 #192.168.8.16
-#501教室/LED
+#client.subscribe(os.environ['REDIS_SUBSCRIBE'],qos=2)
 import paho.mqtt.client as mqtt
 import redis
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-redis_conn = redis.Redis(host='192.168.8.16', port=6379,password='raspberry')
+redis_conn = redis.Redis(host=os.environ['REDIS_HOST'], port=6379,password=os.environ['REDIS_PASSWORD'])
 
 
 def on_message(mosq, obj, msg):
@@ -15,6 +18,7 @@ def on_message(mosq, obj, msg):
 if __name__ == '__main__':
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.on_message = on_message
-    client.connect('192.168.8.16')
-    client.subscribe('501教室/LED燈',qos=2)
+    client.connect(os.environ['MQTT_SERVER'])
+    client.subscribe(os.environ['REDIS_SUBSCRIBE'],qos=2)
+    #client.subscribe("501教室/LED燈",qos=2)
     client.loop_forever()
